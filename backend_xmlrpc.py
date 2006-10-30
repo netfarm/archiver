a21fb3fc6a225b85c2905436752b644675b05839
sherpya <sherpya@netfarm.it>
2006-10-30 15:47:01 +0000

    Update xmlrpc backend to xmlrpclib python code

--- backend_xmlrpc.py
+++ backend_xmlrpc.py
@@ -18,7 +18,6 @@
 # ======================================================================
 ## @file backend_xmlrpc.py
 ## XMLrpc Storage and Archive Backend
-## TODO keepalive?
 
 __doc__ = '''Netfarm Archiver - release 2.0.0 - XmlRpc backend'''
 __version__ = '2.0.0'
@@ -26,10 +25,7 @@ __all__ = [ 'Backend' ]
 
 from archiver import *
 from sys import exc_info
-try:
-    from xmlrpc import client, fault, setLogLevel, setLogger
-except:
-    raise Exception, 'Import error you may need pyxmlrc from http://sourceforge.net/projects/py-xmlrpc'
+from xmlrpclib import ServerProxy, Error
 from urlparse import urlparse
 from time import mktime
 
@@ -37,33 +33,6 @@ from time import mktime
 class BadUrlSyntax(Exception):
     """BadUrlSyntax Bad url syntax in config file"""
     pass
-
-def decode_url(url):
-    """Parses url from config file
-    @param url: is like http://username:password@host:8080/InsertMail/insert
-    @return: a tuple hostame, port, url, username, password"""
-    res = urlparse(url)
-    username, password = None, None
-    if res[0].lower() != 'http':
-        raise BadUrlSyntax
-
-    if res[1].find('@')!=-1:
-        try:
-            auth_str, host_str = res[1].split('@', 1)
-            username, password = auth_str.split(':', 1)
-        except:
-            raise BadUrlSyntax, url
-    else:
-        host_str = res[1]
-
-    try:
-        hostname, port = host_str.split(':', 1)
-        port = int(port)
-    except:
-        raise BadUrlSyntax, url
-
-    url = res[2]
-    return hostname, port, url, username, password
 
 class Backend(BackendBase):
     """XMLrpc Backend using python-xmlrpc
@@ -75,34 +44,30 @@ class Backend(BackendBase):
         self.type = stage_type
         self.LOG = ar_globals['LOG']
         try:
-            setLogLevel(config.getint(self.type, 'debuglevel'))
-            setLogger(self.LOG['log_fd'])
+            self.url = config.get(self.type, 'url')
+            self.method = config.get(self.type, 'method')
+            self.server = ServerProxy(self.url)
         except:
-            setLogLevel(0)
-        url = config.get(self.type, 'url')
-        self.hostname, self.port, self.url, self.username, self.password = decode_url(url)
-        self.client = client(self.hostname, self.port)
-        self.LOG(E_ALWAYS, 'XmlRpc Backend (%s) at %s port %d url is %s' %
-                 (self.type, self.hostname, self.port, self.url))
+            raise BadConfig, 'Bad config in xmlrpc backend'
+
+        self.LOG(E_ALWAYS, 'XmlRpc Backend (%s) at %s' % (self.type, self.url))
 
     def process(self, data):
         """Archive backend proces
         @param data: The data argument is a dict containing mail info and the mail itself
         @return: year as status and pid as code"""
-        ## Fixup Date - xmlrpc cannot encode time.struct_time objects
+        ## FIXME wrap with xmlrpc DateTime - time.struct_time objects cannot be marshalled
         data['m_date'] = mktime(data['m_date'])
         self.LOG(E_TRACE, 'XmlRpc Backend (%s): ready to process %s' % (self.type, data))
         try:
-            status, code = self.client.execute(self.url, [data], -1.0, self.username, self.password)
-            return status, code, 'Ok'
-        except:
-            t, val, tb = exc_info()
-            del tb
-            if isinstance(val, fault):
-                val = val.faultString
-            return 0, 443, '%s: %s' % (t, val)
+            getattr(self.server, self.method)({'data': data})
+        except Error, v:
+            del v ## FIXME Fill error
+            return 0, 443, 'Error'
+
+        return 0, 200, 'Ok'
 
     def shutdown(self):
         """Backend Shutdown callback"""
         self.LOG(E_ALWAYS, 'XmlRpc Backend (%s): closing connection' % self.type)
-        self.client = None
+        self.server = None
