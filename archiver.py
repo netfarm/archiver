1f817adac9867bd6541ece54333c484f8102b265
sherpya <sherpya@netfarm.it>
2007-01-19 18:00:52 +0000

    uffa

--- archiver.py
+++ archiver.py
@@ -514,9 +514,11 @@ def StageHandler(config, stage_type):
 
                 if mid is not None and self.hashdb.has_key(mid):
                     LOG(E_TRACE, '%s-sendmail: expunging msg %s from hashdb' % (self.type, aid))
-                    if self.hashdb.has_key(mid):
+                    try:
                         del self.hashdb[mid]
                         self.hashdb.sync()
+                    except:
+                        pass
 
                 return self.do_exit(250, okmsg, 200)
             else:
@@ -536,8 +538,11 @@ def StageHandler(config, stage_type):
                     return self.do_exit(443, 'All recipients were rejected by the mailserver')
 
                 LOG(E_TRACE, '%s-sendmail: expunging msg %s from hashdb' % (self.type, aid))
-                del self.hashdb[mid]
-                self.hashdb.sync()
+                try:
+                    del self.hashdb[mid]
+                    self.hashdb.sync()
+                except:
+                    pass
                 return self.do_exit(200, 'Some of recipients were rejected by the mailserver')
 
         def do_exit(self, code, msg='', extcode=None):
