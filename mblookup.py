9278cd21c1acb19db3e81859b01a746e33b3befb
sherpya <sherpya@netfarm.it>
2007-04-18 12:38:10 +0000

    New mblookup

--- mblookup.py
+++ mblookup.py
@@ -16,61 +16,54 @@
 # for more details.
 # ======================================================================
 ## @file compress.py
-## Helper for file compression
+## Helper MailBox Lookup using postfix
+
+from anydbm import open as opendb
+from types import ListType
 
 __doc__ = '''Netfarm Archiver - release 2.1.0 - Postfix mailbox lookup'''
 __version__ = '2.1.0'
 __all__ = [ 'mblookup' ]
 
-from anydbm import open as opendb
-from sys import platform
+## Recurse in aliases
+def lookup_alias(entry, db, bc):
+    entries = entry[:-1].strip().split(',')
+    results = []
 
-aliases = '/etc/postfix/aliases.db'
-virtual = '/etc/postfix/virtual.db'
+    for entry in entries:
+        key = entry.strip() + '\x00'
+        value = db['aliases'].get(key)
+        if value is not None:
+            ## Loop detection
+            if value in bc: return []
+            bc.append(value)
+            results = results + lookup_alias(value, db, bc)
+        else:
+            results = results + [entry.strip()]
+    return results
 
-def lookup_alias(dba, bc, alias):
-    if alias.find('@') != -1: return [] # External
-    al = dba.get(alias, None)
-    if al is None:
-        return [alias[:-1].strip()]
-
-    alist = al[:-1].strip().split(',')
-    if len(alist) == 1: return [al[:-1].strip()]
-    res = []
-    for al in alist:
-        al = al.strip()
-        if al in bc: return [] # Loop
-        bc.append(al)
-        al = al + '\x00'
-        res = res + lookup_alias(dba, bc, al)
-    return res
-
-def lookup(dba, dbv, email):
-    email = email + '\x00'
-    mbox = dbv.get(email, None)
-
-    if mbox is None: # No match
-        return []
-    if mbox.find('@') != -1: # External
-        return []
+## Lookup in virtual
+def lookup(email, db):
+    key = email.strip() + '\x00'
+    value = db['virtual'].get(key)
+    if value is None or value.find('@') != -1: return []
     bc = []
-    return lookup_alias(dba, bc, mbox)
+    return lookup_alias(value, db, bc)
 
-# Postfix db files
-def mblookup(emails):
+def getusers(emails, db):
     if platform == 'win32': return [] # assume no mailbox on win32
 
-    dba = opendb(aliases, 'r')
-    dbv = opendb(virtual, 'r')
-
+    if (type(emails) != ListType) or (len(emails) < 1): return []
+    results = []
     res = []
+
     for email in emails:
-        res = res + lookup(dba, dbv, email)
+        res = res + lookup(email, db)
 
-    dba.close()
-    dbv.close()
+    # Uniq + Sort
+    for r in res:
+        if not r in results:
+            results.append(r)
+    results.sort()
 
-    # Unique mbox
-    result = {}
-    for r in res: result[r] = True
-    return result.keys()
+    return results
