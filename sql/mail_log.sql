3334adda77e27a9508583399898a3797d267670b
sherpya <sherpya@netfarm.it>
2007-07-12 13:56:49 +0000

    revised db

--- sql/mail_log.sql
+++ sql/mail_log.sql
@@ -1,19 +1,28 @@
-create table mail_log (
+create table mail_log_in (
     id serial primary key,
+    mailfrom varchar(256),
     message_id varchar(512) not null,
     r_date timestamp not null,
+    mail_size integer not null default 0,
+    nrcpts smallint not null default 0,
+    ref varchar(256) not null
+);
+
+create index idx_mail_log_in_ref on mail_log_in using hash (ref);
+create index idx_mail_log_message_id on mail_log_in using hash (message_id);
+create index idx_mail_log_r_date on mail_log_in using btree (r_date);
+
+create table mail_log_out (
+    mail_id integer not null references mail_log_in(id),
     d_date timestamp not null,
     dsn varchar(16) not null,
     relay varchar(256) not null,
     delay numeric not null default 0,
-    status varchar(64) not null default 'N.D.',
+    status varchar(64) not null default 'unknown',
     status_desc text not null default '',
-    mailto varchar(256) not null default 'N.D.',
-    ref varchar(256) not null default ''
+    mailto varchar(256)
 );
-create index idx_mail_log_message_id on mail_log (message_id);
-create index idx_mail_log_r_date on mail_log (r_date);
-create index idx_mail_log_d_date on mail_log (d_date);
-create index idx_mail_log_relay on mail_log (relay);
-create index idx_mail_log_mailto on mail_log (mailto);
-create index idx_mail_log_ref on mail_log (ref);
+
+create index idx_mail_log_out_mail_id on mail_log_out using btree (mail_id);
+create index idx_mail_log_out_d_date on mail_log_out using btree (d_date);
+create index idx_mail_log_out_mailto on mail_log_out using btree (mailto);
